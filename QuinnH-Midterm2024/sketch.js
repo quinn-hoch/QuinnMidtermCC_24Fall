c6e20fd237726bc2649394ca3c6f2c9cbd7efe8a
quinn-hoch <qh2202@nyu.edu>
2024-10-21 15:00:23 -0400

    second commit?

---
+++ QuinnH-Midterm2024/sketch.js
@@ -0,0 +1,14 @@
+//MIDTERM PSEUDO CODE
+function setup() {
+createCanvas(900, 900);
+//background() beige
+}
+
+function draw() {
+//opacity needs to slowly develop then add more cracks?
+
+}
+
+if(mousePressed()){
+  ellipse(mouseX, mouseY, 10, 10);
+}
